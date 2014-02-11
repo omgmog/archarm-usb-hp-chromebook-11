ad5689cc13f45759455d234ae3a24503314a1e12
Max Glenister <moglenstar@gmail.com>
2014-02-11 12:42:59 +0000

    Added lighted installation

--- post-install.sh
+++ post-install.sh
@@ -4,6 +4,8 @@ REPOPATH="https://raw2.github.com/omgmog/archarm-usb-hp-chromebook-11/master/"
 # Update pacman and install some important things
 pacman -Syy
 pacman -S mate mate-extra xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-armsoc-chromium xf86-input-synaptics
+pacman -S lightdm lightdm-gtk2-greeter
+systemctl enable lightdm
 echo "exec mame-session" > ~/.xinitrc
 
 # Add xorg.conf entries for screen and touchpad
