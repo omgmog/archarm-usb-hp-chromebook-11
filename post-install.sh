cde5ec1c2cfac02b07105222c19f5379297ebf6d
Max Glenister <moglenstar@gmail.com>
2014-07-31 16:48:47 +0100

    Changed post-install to use xf86-video-fbdev over armsoc

--- post-install.sh
+++ post-install.sh
@@ -3,7 +3,7 @@ REPOPATH="https://raw2.github.com/omgmog/archarm-usb-hp-chromebook-11/master/"
 
 # Update pacman and install some important things
 pacman -Syyu
-pacman -S mate xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-armsoc-chromium xf86-input-synaptics
+pacman -S mate xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-fbdev xf86-input-synaptics
 pacman -S lightdm lightdm-gtk2-greeter
 systemctl enable lightdm
 echo "exec mame-session" > ~/.xinitrc
