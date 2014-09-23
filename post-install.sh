997b4f17377a4bf57f841a94888dc56093c69fb3
Max Glenister <moglenstar@gmail.com>
2014-09-23 08:10:24 +0100

    Fixed typo in post-install.sh
    Closes #11

--- post-install.sh
+++ post-install.sh
@@ -6,7 +6,7 @@ pacman -Syyu
 pacman -S mate xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-fbdev xf86-input-synaptics
 pacman -S lightdm lightdm-gtk2-greeter
 systemctl enable lightdm
-echo "exec mame-session" > ~/.xinitrc
+echo "exec mate-session" > ~/.xinitrc
 
 # Add xorg.conf entries for screen and touchpad
 cd /etc/X11/xorg.conf.d/
