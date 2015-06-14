97ed06794fd2d78013071ab9ec5b7ce74369968f
Alkin Nasuf <nasufster@me.com>
2015-06-15 00:08:03 +0300

    Copy alsa state files to /opt

--- post-install.sh
+++ post-install.sh
@@ -9,11 +9,16 @@ then
 fi
 pacman -S mesa-libgl mate xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-fbdev xf86-input-synaptics
 pacman -S lightdm lightdm-gtk2-greeter
+pacman -S alsa-lib alsa-utils
 systemctl enable lightdm
 echo "exec mate-session" > ~/.xinitrc
+cp /opt/asound.state /etc/asound.state
+cp -r /opt/ucm* /usr/share/alsa/ucm/
+
 alsactl -F -f /etc/asound.state restore
 alsaucm -c DAISY-I2S
 
+rm -rf /opt/*
 # Add xorg.conf entries for screen and touchpad
 cd /etc/X11/xorg.conf.d/
 wget ${REPOPATH}10-monitor.conf
