213c70dc276c62e7bffd3c311a856edd6d650e2b
wamserma <markus-wamser@kleine-prinzen.de>
2015-10-06 23:34:02 +0200

    Removed mate desktop, added other packages.

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPOPATH="https://raw.githubusercontent.com/omgmog/archarm-usb-hp-chromebook-11/master/"
+REPOPATH="https://raw.githubusercontent.com/wamserma/archarm-usb-hp-chromebook-11/master/"
 
 # Update pacman and install some important things
 pacman -Syyu
@@ -7,12 +7,17 @@ if !(which wget)
 then
 	pacman -Sy wget
 fi
-pacman -S mesa-libgl mate xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-fbdev xf86-input-synaptics
-pacman -S lightdm lightdm-gtk2-greeter
-systemctl enable lightdm
-echo "exec mate-session" > ~/.xinitrc
+pacman -S lxde xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-fbdev xf86-input-synaptics
+#pacman -S lightdm lightdm-gtk2-greeter
+#systemctl enable lightdm
+#echo "exec mate-session" > ~/.xinitrc
 
 # Add xorg.conf entries for screen and touchpad
 cd /etc/X11/xorg.conf.d/
 wget ${REPOPATH}10-monitor.conf
 wget ${REPOPATH}50-touchpad.conf
+
+# other useful programs/software I use often
+pacman -S geany
+pacman -S llvm 
+pacman -S haskell
