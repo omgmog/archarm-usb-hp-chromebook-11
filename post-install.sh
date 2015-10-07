acc59a4802c112f80affcb1f78cdb09b7b43da98
wamserma <markus-wamser@kleine-prinzen.de>
2015-10-08 00:03:05 +0200

    Added more packages

--- post-install.sh
+++ post-install.sh
@@ -8,9 +8,7 @@ then
 	pacman -Sy wget
 fi
 pacman -S lxde xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-fbdev xf86-input-synaptics
-#pacman -S lightdm lightdm-gtk2-greeter
-#systemctl enable lightdm
-#echo "exec mate-session" > ~/.xinitrc
+echo "exec startlxde" > ~/.xinitrc
 
 # Add xorg.conf entries for screen and touchpad
 cd /etc/X11/xorg.conf.d/
@@ -18,6 +16,7 @@ wget ${REPOPATH}10-monitor.conf
 wget ${REPOPATH}50-touchpad.conf
 
 # other useful programs/software I use often
-pacman -S geany
+pacman -S screen packer base-devel git
+pacman -S geany libreoffice-fresh
 pacman -S llvm 
-pacman -S haskell
+pacman -S ghc
