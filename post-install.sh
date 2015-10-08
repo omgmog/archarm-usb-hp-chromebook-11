ae88df0349d93c3e031f930cdcf39d9291edfeea
wamserma <markus-wamser@kleine-prinzen.de>
2015-10-09 01:18:42 +0200

    Added more packages

--- post-install.sh
+++ post-install.sh
@@ -7,9 +7,12 @@ if !(which wget)
 then
 	pacman -Sy wget
 fi
-pacman -S lxde xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-fbdev xf86-input-synaptics
+pacman -S ttf-dejavu lxde xorg-server xorg-xinit xorg-server-utils xterm alsa-utils xf86-video-fbdev xf86-input-synaptics
 echo "exec startlxde" > ~/.xinitrc
 systemctl enable lxdm.service
+
+pacman -S wicd wicd-gtk
+systemctl enable wicd
 
 # Add xorg.conf entries for screen and touchpad
 cd /etc/X11/xorg.conf.d/
@@ -17,7 +20,7 @@ wget ${REPOPATH}10-monitor.conf
 wget ${REPOPATH}50-touchpad.conf
 
 # other useful programs/software I use often
-pacman -S screen packer base-devel git
-pacman -S geany libreoffice-fresh
+pacman -S screen packer base-devel git gnupg openvpn wicd wicd-gtk
+pacman -S geany libreoffice-fresh firefox mutt
 pacman -S llvm 
-pacman -S ghc
+
